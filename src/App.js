bc4800017c2170c6805caa10962cbd33b3935472
saqibk999 <saqib.mohd1@byjus.com>
2023-08-24 14:23:48 +0530

    debugging v6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import { useEffect } from 'react';
 import './App.css';
-import { requestPermission } from './firebase';
 import ChatBox from './components/ChatBox';
 
 function App() {
-
-  useEffect(() => {
-     requestPermission();
-  },[])
 
   return (
     <div className="App">
